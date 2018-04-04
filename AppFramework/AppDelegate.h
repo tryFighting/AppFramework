4750b7058b72f10b6cfd4cdec41d8cada95a6b47
tryFighting <2893381320@qq.com>
2018-04-04 10:10:48 +0800

    初始化项目到GitHub

---
+++ AppFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppFramework
+//
+//  Created by zrq on 2018/4/4.
+//  Copyright © 2018年 zrq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppFramework
+//
+//  Created by zrq on 2018/4/4.
+//  Copyright © 2018年 zrq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
